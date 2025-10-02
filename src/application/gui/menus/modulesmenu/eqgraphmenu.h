3d31e7d14a86877b9c0aa0e7fb1496fb67dc01a1
Dmitriy Kostiuchik <uaas@list.ru>
2025-10-02 15:32:38 +0300

    PRE-POST new EQ menu

--- src/application/gui/menus/modulesmenu/eqgraphmenu.h
+++ src/application/gui/menus/modulesmenu/eqgraphmenu.h
@@ -27,12 +27,12 @@ private:
 	bool updateResponse{false};
 	void printPage();
 
-	int8_t* eqFreq_ptr[7];
-	int8_t minParamVal[7];
-	int8_t maxParamVal[7];
+	int8_t* eqFreq_ptr[8];
+	int8_t minParamVal[8];
+	int8_t maxParamVal[8];
 
-	static constexpr uint8_t paramNames[][3] = {"B:", "F:", "G:", "Q:"};
-	static constexpr uint8_t bandNames[][4] = {" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " HP", " LP"};
+	static constexpr uint8_t paramNames[][3] = {"P:", "F:", "G:", "Q:"};
+	static constexpr uint8_t bandNames[][6] = {" B1  ", " B2  ", " B3  ", " B4  ", " B5  ", " HP  ", " LP  ", " POS."};
 };
 
 #endif /* SRC_APPLICATION_GUI_MENUS_MODULESMENU_EQGRAPHMENU_H_ */
